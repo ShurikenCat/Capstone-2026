6ff2122cadf468cc625a02a642321d55d93f1a29
Thomas Mullens <tmullens@growthaccelerationpartners.com>
2026-02-18 10:00:02 -0600

    Howdy

--- app/src/main/java/com/example/capstone2026/MainActivity.kt
+++ app/src/main/java/com/example/capstone2026/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Capstone2026Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Hello World",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Howdy, $name!",
         modifier = modifier
     )
 }
