0675b59ca4ccfa2a9e41310d6a69e962b84414cd
dylan <dylan47.nguyen@gmail.com>
2026-03-01 17:06:32 -0600

    Added parts of front end

--- app/src/main/java/com/example/capstone2026/MainActivity.kt
+++ app/src/main/java/com/example/capstone2026/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.capstone2026.ui.theme.Capstone2026Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +30,229 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Capstone2026Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Hello World",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Hello World",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+                ScheduleScreen()
+            }
+        }
+    }
+}
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Howdy, $name!",
+//        modifier = modifier.padding(16.sp)
+//    ){
+//        Text(
+//            text = "Login",
+//            style = MaterialTheme.typography.headlineMedium
+//        )
+//
+//
+//    }
+//}
+
+@Composable
+fun LoginScreen(){
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var message by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Login",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Email Input
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        // Password Input
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Login Button
+        Button(
+            onClick = {
+                if (email == "test@example.com" && password == "1234") {
+                    message = "Login Successful!"
+                } else {
+                    message = "Invalid credentials"
                 }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Login")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+
+            },
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text("Login with Google")
+        }
+
+        // Result Message
+        if (message.isNotEmpty()) {
+            Text(
+                text = message,
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+}
+
+@Composable
+fun ScheduleScreen(){
+    var isExpanded by remember { mutableStateOf(false) }
+
+//    Column(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .padding(24.dp),
+//        verticalArrangement = Arrangement.Center,
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ){
+//
+//    }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Button(
+            onClick = {
+                isExpanded = !isExpanded
+            },
+            modifier = Modifier
+                .size(80.dp)
+                .padding(vertical = 8.dp, horizontal = 8.dp)
+                .align(Alignment.BottomEnd),
+            shape = CircleShape,
+
+        ){
+            Text(
+                text = "+",
+                fontSize = 40.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 100.dp), // Push menu above button
+            horizontalAlignment = Alignment.End
+        ) {
+
+            AnimatedVisibility(
+                visible = isExpanded,
+                enter = fadeIn() + slideInVertically { it / 2 },
+                exit = fadeOut() + slideOutVertically { it / 2 }
+            ) {
+                SpeedDialItem(
+                    text = "Test 1",
+                    onClick = {
+                        isExpanded = false
+                    }
+                )
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            AnimatedVisibility(
+                visible = isExpanded,
+                enter = fadeIn() + slideInVertically { it / 2 },
+                exit = fadeOut() + slideOutVertically { it / 2 }
+            ) {
+                SpeedDialItem(
+                    text = "Test 2",
+                    onClick = {
+                        isExpanded = false
+                    }
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Howdy, $name!",
-        modifier = modifier
-    )
+fun SpeedDialItem(
+    text: String,
+    onClick: () -> Unit
+) {
+    Surface(
+        onClick = onClick,
+        shape = RoundedCornerShape(24.dp),
+        tonalElevation = 6.dp,
+        shadowElevation = 6.dp
+    ) {
+        Text(
+            text = text,
+            modifier = Modifier.padding(
+                horizontal = 20.dp,
+                vertical = 12.dp
+            )
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LoginScreenPreview(){
     Capstone2026Theme {
-        Greeting("Android")
+        LoginScreen()
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun ScheduleScreenPreview(){
+    Capstone2026Theme {
+        ScheduleScreen()
+    }
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    Capstone2026Theme {
+//        Greeting("Android")
+//    }
+//}
